1033a1d82de33d2b568b4633c8c735fa8af412e6
MD. MAHIDUL MOLLA <106535071+AadittoMahidul@users.noreply.github.com>
2023-02-04 23:53:51 +0600

    MVCPractice01
    For Autosoft Practice self

---
+++ Practice_01/Controllers/EmployeesController.cs
@@ -0,0 +1,65 @@
+﻿using Practice_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice_01.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        EmployeeDbContext db = new EmployeeDbContext();
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employees = db.Employees.ToList();
+            return View(employees);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.emp = db.Employees.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.emp = db.Employees.ToList();
+            return View(emp);
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(db.Employees.First(x => x.EmployeeId == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");   
+            }
+            ViewBag.emp = db.Employees.ToList();
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            return View(db.Employees.First(x=>x.EmployeeId ==id));
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            Employee emp = new Employee { EmployeeId = id };
+            db.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Practice_01/Models/DbModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practice_01.Models
+{
+    public enum Department { Admin=1, Marketing, Software, Staff}
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Department Department { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsContinue { get; set; }
+    }
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext()
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<EmployeeDbContext>
+    {
+        protected override void Seed(EmployeeDbContext db)
+        {
+            if (!db.Employees.Any())
+            {
+                db.Employees.Add(new Employee { EmployeeName = "MM", JoinDate = new DateTime(2022, 09, 01), Department = Department.Admin, Salary = 50000.00M, IsContinue = true });
+                db.Employees.Add(new Employee { EmployeeName = "NN", JoinDate = new DateTime(2021, 10, 01), Department = Department.Marketing, Salary = 40000.00M, IsContinue = false });
+                db.SaveChanges();
+            }
+        }
+    }
+}
